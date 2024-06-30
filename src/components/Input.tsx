575d2c7349753350865fc09e2aab872e91ed4008
Aibek <aibekm591@gmail.com>
2024-06-30 23:25:23 +0600

    chore:Добавил input error, description, variant

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,23 +1,79 @@
 import React from 'react';
 import {TextField} from "@mui/material";
+import {Simulate} from "react-dom/test-utils";
+import error = Simulate.error;
 
 
 interface PropsInput {
     type?: string,
     placeholder: string,
-    value: string,
+    value?: string,
     onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void,
-    label?: string
+    label?: string,
+    description?: string,
+    nickname?: string
+    error?: boolean
+    errorValue?: string
+    variant?: 'filled' | 'standard' | 'outlined'
+    radius?: number
+    size?: "small" | "medium" | undefined,
+    disabled?: boolean
+    asterisk?: boolean
+    icon?: React.ReactNode
+    className?: string,
+    color?: 'primary'
+        | 'secondary'
+        | 'error'
+        | 'info'
+        | 'success'
+        | 'warning'
 }
 
-const Input = ({type,placeholder,value,onChange,label}: PropsInput) => {
+const Input = (
+    {
+        type,
+        placeholder,
+        value,
+        onChange,
+        label,
+        description,
+        error,
+        size,
+        disabled,
+        nickname,
+        asterisk,
+        variant,
+        radius,
+        icon,
+        className,
+        color,
+        errorValue
+    }: PropsInput) => {
     return (
-        <TextField
-            value={value}
-            placeholder={placeholder}
-            type={type}
-            onChange={onChange}
-        />
+        <>
+            <p>{description}</p>
+            <label style={{
+                display: 'flex',
+                flexDirection: 'column'
+
+            }} className={className}>
+                {label}
+                {asterisk && <span style={{color: "red"}}>*</span>}
+                <TextField
+                    value={value}
+                    placeholder={placeholder}
+                    type={type}
+                    onChange={onChange}
+                    variant={variant}
+                    disabled={disabled}
+                    error={error}
+                    size={size}
+                    color={color}
+                />
+                <span>{icon}</span>
+                <span style={{color: 'red'}}>{errorValue}</span>
+            </label>
+        </>
     );
 };
 
--- src/components/SignRegister.tsx
+++ src/components/SignRegister.tsx
@@ -1,13 +1,11 @@
 import React, {useState} from 'react';
 import Input from "./Input";
-import {Button} from "@mui/material";
+import {Button, Switch} from "@mui/material";
 import './register.css'
 import {Link} from "react-router-dom";
 
 
-
 const SignRegister = () => {
-    
 
 
     const [name, setName] = useState('');
@@ -16,18 +14,14 @@ const SignRegister = () => {
     const [gender, setGender] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
+    const [disabled, setDisabled] = useState(false);
+    const [asterisk, setAsterisk,] = useState(false);
+    const [description, setDescription] = useState('')
+    const [errorValue, setErrorValue] = useState('');
+    const [variant, setVariant] = useState<'filled' | 'standard' | 'outlined' | undefined>(undefined)
 
     const handleRegister = (e: React.ChangeEvent<HTMLFormElement>) => {
         e.preventDefault()
-        console.log({
-            name,
-            nick,
-            email,
-            gender,
-            password,
-            confirmPassword
-        })
-
     }
 
     const handleReset = () => {
@@ -37,32 +31,69 @@ const SignRegister = () => {
         setGender('')
         setPassword('')
         setConfirmPassword('')
+        setDescription('')
+        setErrorValue('')
     }
 
+
+    const handleChangedSwitched = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setDisabled(e.target.checked)
+    }
+
+    const handleChangedAsterisk = (e: React.ChangeEvent<HTMLInputElement>) => {
+        setAsterisk(e.target.checked)
+    }
+
+
+
     return (
+        <div className={'wrapper'}>
+            <div className={'buttons'}>
+                <Button variant={'outlined'} onClick={() => setVariant('filled')}>filled</Button>
+                <Button variant={'outlined'} onClick={() => setVariant('standard')}>standard</Button>
+                <Button variant={'outlined'} onClick={() => setVariant('outlined')}>outlined</Button>
+            </div>
+            <form
+                className={'form'}
+                onSubmit={handleRegister}
+                onReset={handleReset}
+            >
+                <Input variant={variant} asterisk={asterisk} disabled={disabled} type={'text'} placeholder={'name'}
+                       value={name}
+                       onChange={(e) => setName(e.target.value)}/>
+                <Input variant={variant} asterisk={asterisk} disabled={disabled} type={'text'} placeholder={'nickName'} value={nick}
+                       onChange={(e) => setNick(e.target.value)}/>
+                <Input variant={variant} asterisk={asterisk} disabled={disabled} type={'email'} placeholder={'email'} value={email}
+                       onChange={(e) => setEmail(e.target.value)}/>
+                <Input variant={variant} asterisk={asterisk} disabled={disabled} type={'text'} placeholder={'gender'} value={gender}
+                       onChange={(e) => setGender(e.target.value)}/>
+                <Input variant={variant} asterisk={asterisk} disabled={disabled} type={'password'} placeholder={'password'}
+                       value={password}
+                       onChange={(e) => setPassword(e.target.value)}/>
+                <Input variant={variant} asterisk={asterisk} disabled={disabled} type={'password'} placeholder={'confirmPassword'}
+                       value={confirmPassword}
+                       onChange={(e) => setConfirmPassword(e.target.value)}/>
+                <Input variant={variant} asterisk={asterisk} disabled={disabled} placeholder={'description'} description={description}
+                       value={description}
+                       onChange={(e) => setDescription(e.target.value)}/>
+                <Input variant={variant} asterisk={asterisk} disabled={disabled} errorValue={errorValue} color={'warning'}
+                       placeholder={'error'}
+                       value={errorValue} onChange={(e) => setErrorValue(e.target.value)}
+                       error={Boolean(errorValue) && true}/>
+                <Switch
+                    onChange={handleChangedSwitched}
+                />
+                <Switch
+                    onChange={handleChangedAsterisk}
+                />
+                <Button type={"submit"}>Зарегистрироваться</Button>
+                <Button type={"reset"}>Сброс</Button>
+            </form>
+            <p className={'text'}>Уже есть аккаунт ? <Link to={'/'}>перейти</Link></p>
+        </div>
 
-        <form
-            className={'form'}
-            onSubmit={handleRegister}
-            onReset={handleReset}
-        >
-            <Input type={'text'} placeholder={'name'} value={name} onChange={(e) => setName(e.target.value)}/>
-            <Input type={'text'} placeholder={'nickName'} value={nick} onChange={(e) => setNick(e.target.value)}/>
-            <Input type={'email'} placeholder={'email'} value={email} onChange={(e) => setEmail(e.target.value)}/>
-            <Input type={'text'} placeholder={'gender'} value={gender} onChange={(e) => setGender(e.target.value)}/>
-            <Input type={'password'} placeholder={'password'} value={password}
-                   onChange={(e) => setPassword(e.target.value)}/>
-            <Input type={'password'} placeholder={'confirmPassword'} value={confirmPassword}
-                   onChange={(e) => setConfirmPassword(e.target.value)}/>
-            <Button type={"submit"}>Зарегистрироваться</Button>
-            <Button type={"reset"}>Сброс</Button>
-
-            <p>Уже есть аккаунт ? <Link to={'/'}>перейти</Link></p>
-
-
-        </form>
-
-    );
+    )
+        ;
 };
 
 export default SignRegister;
